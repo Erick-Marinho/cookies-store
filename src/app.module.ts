54ead0434bee42dde35f3508074dec96d4ca869c
Erick-Marinho <erickgaves@gmail.com>
2022-02-24 02:47:46 -0300

    :sparkles: Configurando Bando de Dados

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,12 @@ import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import serverConfig from './config/server.config';
 import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [
     UserModule,
+    MongooseModule.forRoot('mongodb://localhost/cookie-store'),
     ConfigModule.forRoot({
       isGlobal: true,
       load: [serverConfig],
