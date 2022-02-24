169c4f6bbc21c1825e18ef39b3aa47e5ec655834
Erick-Marinho <erickgaves@gmail.com>
2022-02-24 02:48:46 -0300

    :sparkles: Cadastro de usuários e implementação de dto e entity

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,11 +1,24 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { Model } from 'mongoose';
+import { InjectModel } from '@nestjs/mongoose';
+import { UserDocument } from 'src/schemas/user.schema';
+import { User } from './entities/user.entity';
 
 @Injectable()
 export class UserService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {}
+
+  async create(createUserDto: CreateUserDto) {
+    const { email } = createUserDto;
+    const user = new this.userModel(createUserDto);
+    const findUser = await this.userModel.findOne({ email });
+    if (findUser) {
+      throw new ConflictException('Usuário já cadastrado');
+    }
+    user.save();
+    return { message: 'Usuário cadastrado com sucesso' };
   }
 
   findAll() {
